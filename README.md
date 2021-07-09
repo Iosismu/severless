a024b645ee8276f78eeb6fa13453d52cb5ed3808
Iosismu <evanpark333@gmail.com>
2021-07-09 15:42:31 +0900

    serverless

---
+++ README.md
@@ -0,0 +1,38 @@
+# 티켓 예매 프로젝트 (AWS Serverless Project)
+
+[Notion Docs](https://www.notion.so/Serverless-d28b19c7b3404579b5fc58a28ed8aee5) <br>
+
+[사이트 바로가기](https://d32irlhdxs9v1a.cloudfront.net/)
+
+- AWS의 각 서비스가 어떤 역활, 설정, 운영 되는지 배우고 프로젝트를 통해서 직접 구축, 배포 했습니다.
+- BLACKPINK 티켓 예매 프로젝트, 간단한 UI를 구성하고 Severless구축에 집중 하였습니다.
+
+## 💡 Specs
+
+- HTML, CSS, JS
+- Ajax
+- Material Design Lite
+- Python
+- S3
+- CloudFront
+- Lambda
+- SNS
+- API Gateway
+- DynamoDB
+- Doker, Docker Compose
+
+# AWS Serverless 아키텍쳐
+
+<p align='center'>
+  <img src='./blackpinkT/images/Lambda.PNG'>
+</p>
+
+# <자세한 기능 설명>
+
+## 1. 메인 화면
+
+<p align='center'>
+  <img src='./images/안드2.PNG'>
+</p>
+
+- 메인 UI에서는 원하는 디퓨저의 상태를 설정 할 수 있습니다.
---
+++ blackpinkT/index.js
@@ -0,0 +1,106 @@
+$(document).ready(function () {
+  var dialog = document.querySelector("dialog");
+  dialogPolyfill.registerDialog(dialog);
+  $(document).on("click", ".show-modal", function (e) {
+    e.preventDefault();
+    var data = $(this).data();
+    var user = data["user"];
+    var type = data["type"];
+    $("#showBox").html(
+      '<img style="width:100%" src="' +
+        "https://d32irlhdxs9v1a.cloudfront.net" +
+        "/qrcodes/" +
+        user +
+        "/" +
+        type +
+        '/qrcode.jpg"/>'
+    );
+    dialog.showModal();
+  });
+  dialog.querySelector(".close").addEventListener("click", function () {
+    dialog.close();
+  });
+  dialog.querySelector(".print").addEventListener("click", function () {
+    print();
+  });
+
+  $("#submitBtn").on("click", function () {
+    submit();
+  });
+
+  function submit() {
+    var user_name = $("#user_name").val();
+    var phone_number = $("#phone_number").val();
+    var company_name = $("#company_name").val();
+    var user_id = $("#user_id").val();
+    var type = $("#type").val();
+    console.log(user_name, phone_number, company_name, user_id, type);
+
+    $.ajax({
+      url: "https://9xtyp7cc06.execute-api.ap-northeast-2.amazonaws.com/dev/conference",
+      method: "post",
+      datatype: "json",
+      data: JSON.stringify({
+        type: type,
+        user_id: user_id,
+        user_name: user_name,
+        phone_number: phone_number,
+        company_name: company_name,
+      }),
+      success: function (r) {
+        console.log(r);
+      },
+      complete: function (r) {
+        console.log(r);
+        location.reload();
+      },
+    });
+  }
+
+  function load_data() {
+    $.ajax({
+      url: "https://9xtyp7cc06.execute-api.ap-northeast-2.amazonaws.com/dev/conference/users/*",
+      method: "get",
+      success: function (r) {
+        var html = "";
+        r["items"].forEach(function (item) {
+          console.log(item);
+          html +=
+            '<li class="mdl-list__item mdl-list__item--three-line"> <span class="mdl-list__item-primary-content"> <i class="material-icons mdl-list__item-avatar">person</i> <span>';
+          html += item["user_name"];
+          html += '</span> <span class="mdl-list__item-text-body">';
+          html += "From. " + item["company_name"] + "<br/> " + item["type"];
+          html +=
+            '</span> </span> <span class="mdl-list__item-secondary-content"> <a data-user="' +
+            item["user_id"] +
+            '" data-type="' +
+            item["type"] +
+            '" class="show-modal mdl-list__item-secondary-action" href="#"><i class="material-icons">print</i></a> </span> </li>';
+        });
+        $("#history").html(html);
+      },
+      complete: function (r) {
+        //console.log(r)
+      },
+    });
+  }
+  load_data();
+});
+
+// <li class="mdl-list__item mdl-list__item--three-line">
+//   <span class="mdl-list__item-primary-content">
+//     <i class="material-icons mdl-list__item-avatar">person</i>
+//     <span>Name</span>
+//     <span class="mdl-list__item-text-body">
+//       From Company Name, Serverless Web project
+//     </span>
+//   </span>
+//   <span class="mdl-list__item-secondary-content">
+//     <a
+//       class="show-modal mdl-list__item-secondary-action"
+//       href="#"
+//     >
+//       <i class="material-icons">print</i>
+//     </a>
+//   </span>
+// </li>;
